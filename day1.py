6e125ecb122531682661932e1068edce38dccd3a
Nihat <nhatyldrm@gmail.com>
2021-03-06 17:40:34 -0500

    finito

--- day1.py
+++ day1.py
@@ -1,1 +1,4 @@
-# The fun begins ...
+print("Welcome")
+city_name =input("Which city you grew up? ")
+pet_name = input("What's the name of your pet? ")
+print("Your band name will be " + pet_name + " " +city_name  )
