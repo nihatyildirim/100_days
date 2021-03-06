46939953b1624cb9767f8be0867cd70500c44acc
Nihat <nhatyldrm@gmail.com>
2021-03-06 17:42:46 -0500

    forget the space on city_name

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
 print("Welcome")
 city_name =input("Which city you grew up? ")
 pet_name = input("What's the name of your pet? ")
-print("Your band name will be " + pet_name + " " +city_name  )
+print("Your band name will be " + pet_name + " " + city_name  )
